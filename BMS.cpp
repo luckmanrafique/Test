0d073786df918be9ec5a5845a45335607d153159
LUCKMAN RAFIQUE <luckmanrafique5427@gmail.com>
2025-10-30 00:22:48 +0600

    Refactor BankAccount class to add PIN and improve details
    Updated the BankAccount class to include a PIN attribute and modified input_details to capture it. Enhanced account number generation and output formatting.

--- BMS.cpp
+++ BMS.cpp
@@ -2,71 +2,88 @@
 #include<fstream>
 #include<string>
 using namespace std;
-class BankAccount{
-    private:
-        string name;
-        string address;
-        string phone;
-        string email;
-        long int account_number;
-        float balance;
-    public:
-        BankAccount(){
-            name = "";
-            address = "";
-            phone = "";
-            email = "";
-            account_number = 0;
-            balance = 0.0;
-        }
-        ~BankAccount(){
-            
-        }
-        void generate_account_number(){
-            ifstream in("AccountNumber.txt");
+
+class BankAccount {
+private:
+    string name;
+    string address;
+    string phone;
+    string email;
+    long int account_number;
+    float balance;
+    int pin;
+
+public:
+    BankAccount(){
+        this->name=" ";
+        this->address=" ";
+        this->phone=" ";
+        this->email=" ";
+        this->account_number=0;
+        this->balance=0.0;
+        this->pin=0;
+    }
+    ~BankAccount(){}
+    void generate_account_number() {
+        ifstream in("AccountNumber.txt");
+        if (!in || !(in >> account_number)) {
             account_number = 20501000;
-            in>>account_number;
-            account_number ++;
-            in.close();
-            ofstream out("AccountNumber.txt");
-            out<<account_number;
-            out.close();
         }
-        void input_details(){
-            ofstream in("Details.txt",ios::app);
-            cout<<"Enter name: ";
-            getline(cin,name);
-            in<< name+"\n";
-            cout<<"Enter address: ";
-            getline(cin,address);
-            in<<address+"\n";
-            cout<<"Enter phone number: ";
-            cin>>phone;
-            in<<"+88"+phone+"\n";
-            cout<<"Enter email: ";
-            cin>>email;
-            in<<email+"\n";
-            generate_account_number();
-            in<<"Account Number: "<<account_number<<"\n";
-            cout<<"Enter initial balance: ";
-            cin>>balance;
-            in<<"Balance: "<<balance<<"\n";
-            in.close();
-            cout<<"-----------------"<<endl;
-            cout<<"You account number is: "<<account_number<<endl;
-            cout<<endl;
-        }
+        in.close();
+        account_number++;
+        ofstream out("AccountNumber.txt");
+        out << account_number;
+        out.close();
+    }
+
+    void input_details() {
+        ofstream out("Details.txt", ios::app);
+
+        cout << "Enter name: ";
+        getline(cin, name);
+        out << "Name: " << name << endl;
+
+        cout << "Enter address: ";
+        getline(cin, address);
+        out << "Address: " << address << endl;
+
+        cout << "Enter phone number: ";
+        cin >> phone;
+        out << "Phone: +88" << phone << endl;
+
+        cout << "Enter email: ";
+        cin >> email;
+        out << "Email: " << email << endl;
+
+        generate_account_number();
+        out << "Account Number: " << account_number << endl;
+
+        cout << "Enter initial balance: ";
+        cin >> balance;
+        out << "Balance: " << balance << endl;
+
+        cout << "Enter your 4 digit PIN: ";
+        cin >> pin;
+        out << "PIN: " << pin << endl;
+
+        out << "-----------------------------" << endl;
+        out.close();
+
+        cout << "\nYour account has been successfully created.\n";
+        cout << "Your account number is: " << account_number << endl;
+    }
 };
-void display_menu(){
-    cout<<"1. Register new account."<<endl;
+
+void display_menu() {
+    cout << "1. Register new account." << endl;
 }
-int main()
-{
+
+int main() {
     BankAccount acc;
     display_menu();
     int choice;
-    cin>>choice;
-    if(choice==1){
+    cin >> choice;
+    if (choice == 1) {
         cin.ignore();
         acc.input_details();
     }
